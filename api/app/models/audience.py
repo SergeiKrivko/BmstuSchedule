b9425ac98bb76ae83b45c9d18250d16efad874d6
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-04-10 03:41:29 +0300

    nullable lks id

--- api/app/models/audience.py
+++ api/app/models/audience.py
@@ -1,18 +1,20 @@
 from typing import TYPE_CHECKING, Optional
+from uuid import UUID
 
-from sqlalchemy import String
+from sqlalchemy import String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.models.base import Base, LksMixin, SyncMixin
+from app.models.base import Base, SyncMixin
 from app.models.many_to_many import schedule_pair_audience
 
 if TYPE_CHECKING:
     from app.models.schedule_pair import SchedulePair
 
 
-class Audience(Base, LksMixin, SyncMixin):
+class Audience(Base, SyncMixin):
     __tablename__ = "audiences"
 
+    lks_id: Mapped[Optional[UUID]] = mapped_column(Uuid, nullable=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
     building: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     map_url: Mapped[Optional[str]] = mapped_column(String, nullable=True)
