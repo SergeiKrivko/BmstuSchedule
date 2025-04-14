bab47a909d5269753f8ffb4f66236761d554cde9
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-04-14 14:48:07 +0300

    get-groups: add get_groups

--- api/app/services/group_svc.py
+++ api/app/services/group_svc.py
@@ -1,9 +1,10 @@
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import Depends
 
 from app.api.schemas.base import GroupBase
+from app.api.schemas.group import GroupList
 from app.db.database import ISessionMaker
 from app.domain.errors import NotFoundError
 from app.repos.group_repo import GroupRepo, group_repo
@@ -24,6 +25,38 @@ class GroupSvc:
                 msg = "Group not found"
                 raise NotFoundError(msg)
         return GroupBase.model_validate(group)
+
+    async def get_groups(
+        self,
+        sessionmaker: ISessionMaker,
+        abbr: Optional[str] = None,
+        course: Optional[str] = None,
+        department: Optional[str] = None,
+        faculty: Optional[str] = None,
+        filial: Optional[str] = None,
+        page: int = 1,
+        size: int = 20,
+    ) -> GroupList:
+        async with sessionmaker() as session:
+            groups, total = await self.group_repo.get_all(
+                session,
+                abbr=abbr,
+                course_abbr=course,
+                department_abbr=department,
+                faculty_abbr=faculty,
+                filial_abbr=filial,
+                page=page,
+                size=size,
+            )
+
+            group_schemas = [GroupBase.model_validate(group) for group in groups]
+
+            return GroupList(
+                items=group_schemas,
+                total=total,
+                page=page,
+                size=size,
+            )
 
 
 @lru_cache(maxsize=1)
